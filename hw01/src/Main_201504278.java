6d350ece8105afdda7c1d4f61641951d24e4cdf6
kcs93023 <kcs93023@gmail.com>
2018-09-13 20:47:09 +0900

    Change Stack&Queue -> Dequeue

--- hw01/src/Main_201504278.java
+++ hw01/src/Main_201504278.java
@@ -1,12 +1,8 @@
 import java.io.*;
 import java.util.Deque;
 import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
 
 public class Main_201504278 {
-    static Queue<String> queue = new LinkedList<>();
-    static Stack<String> stack = new Stack<>();
     static Deque<String> deque = new LinkedList<>();
     public static void main(String args[]) {
         String input_code = new String();
@@ -32,20 +28,20 @@ public class Main_201504278 {
 
             if(operation.equals("print")){
                 tmp = new StringBuilder();
-                while(!queue.isEmpty()) {
-                    tmp.append(queue.remove());
+                while(!deque.isEmpty()) {
+                    tmp.append(deque.remove());
                 }
                     output_code.append("\tprintf(\"%s\", \""+ tmp.toString()+"\");\n");
             } else if (operation.equals("ignore")){
-                while(!queue.isEmpty()){
-                    queue.remove();
+                while(!deque.isEmpty()){
+                    deque.remove();
                 }
             } else{
                 tmp = new StringBuilder();
-                while(!queue.isEmpty()){
-                    tmp.append(queue.remove());
+                while(!deque.isEmpty()){
+                    tmp.append(deque.remove());
                 }
-                queue.add(tmp.toString());
+                deque.add(tmp.toString());
             }
         }
         output_code.append("}");
@@ -58,9 +54,6 @@ public class Main_201504278 {
         String[] replace_char;
         String operation, count;
         boolean control_string = false;
-        while(!queue.isEmpty()){
-            stack.push(queue.remove());
-        }
         if (splited_line.length > 2)
             control_string = true;
         for (int i = 0; i < splited_line.length - 1; i++) {
@@ -71,22 +64,17 @@ public class Main_201504278 {
 
             if (splited_line[i].contains("/")) {
                 replace_char = splited_line[i].split("/");
-                stack.push(stack.pop().replace(replace_char[1], replace_char[0]));
+                deque.push(deque.pop().replace(replace_char[1], replace_char[0]));
             } else if (control_string && i > 0 && splited_line[i].equals("U")) {
-                if (!stack.isEmpty())
-                    stack.push(stack.pop().toUpperCase());
+                    deque.push(deque.pop().toUpperCase());
             } else if (control_string && i > 0 &&  splited_line[i].equals("L")) {
-                if (!stack.isEmpty())
-                    stack.push(stack.pop().toLowerCase());
+                    deque.push(deque.pop().toLowerCase());
             } else {
-                stack.push(splited_line[i]);
+                deque.push(splited_line[i]);
             }
         }
-        while(!stack.empty())
-            queue.add(stack.pop());
-
-        for(int i=0;i<queue.size()-1; i++)
-            queue.add(queue.remove());
+        for(int i=0;i<deque.size()-1; i++)
+            deque.add(deque.remove());
 
         String[] op = splited_line[splited_line.length-1].split(":");
         if(op.length > 1) {
@@ -101,18 +89,15 @@ public class Main_201504278 {
 
                 StringBuilder tmp1 = new StringBuilder();
                 StringBuilder tmp2 = new StringBuilder();
-                while(!queue.isEmpty())
-                    tmp1.append(queue.remove());
+                while(!deque.isEmpty())
+                    tmp1.append(deque.remove());
                 for(int j = 0; j < Integer.parseInt(count); j++)
                     tmp2.append(tmp1.toString());
-                queue.add(tmp2.toString());
+                deque.add(tmp2.toString());
             }
         }
         else
             operation = "";
-
-
-
         return operation;
     }
 
