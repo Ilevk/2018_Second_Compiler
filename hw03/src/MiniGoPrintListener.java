dfec7bbf954a96d24e4080d520bfa6e9ccb6a24b
kcs93023 <kcs93023@gmail.com>
2018-10-16 22:13:24 +0900

    Update MiniGoPrintListner
    대부분 구현 했으나, else 구문 그리고 중괄호가 없는 if 문에 대해서는 아직

--- hw03/src/MiniGoPrintListener.java
+++ hw03/src/MiniGoPrintListener.java
@@ -1,16 +1,12 @@
-import org.antlr.v4.runtime.tree.ParseTreeListener;
 import org.antlr.v4.runtime.tree.ParseTreeProperty;
-
-import java.util.List;
 
 public class MiniGoPrintListener extends MiniGoBaseListener {
     ParseTreeProperty<String> newTexts = new ParseTreeProperty<String>();
+    int depth = 0;
 
     @Override
     public void enterFun_decl(MiniGoParser.Fun_declContext ctx) {
         StringBuilder sb = new StringBuilder();
-//        System.out.println("Hello enter fun decl");
-//        String params = newTexts.get(ctx.params());
         sb.append(ctx.FUNC().getText());
         sb.append(" ");
         sb.append(ctx.IDENT().getText());
@@ -19,9 +15,16 @@ public class MiniGoPrintListener extends MiniGoBaseListener {
     }
 
     @Override
+    public void enterIf_stmt(MiniGoParser.If_stmtContext ctx) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(ctx.IF().getText());
+        newTexts.put(ctx, sb.toString());
+        System.out.print(newTexts.get(ctx));
+    }
+
+
+    @Override
     public void enterParams(MiniGoParser.ParamsContext ctx) {
-//        System.out.println("Hello enter param");
-//        List<MiniGoParser.ParamContext> params = ctx.param();
         StringBuilder sb = new StringBuilder();
         sb.append("(");
         for (int i = 0; i < ctx.param().size(); i++) {
@@ -31,8 +34,6 @@ public class MiniGoPrintListener extends MiniGoBaseListener {
             if (i != ctx.param().size() - 1)
                 sb.append(", ");
 
-//            newTexts.put(ctx, ctx.param(0).IDENT().getText());
-//            newTexts.put(ctx,ctx.param(0).type_spec().getText());
         }
         newTexts.put(ctx, sb.toString());
         System.out.print(newTexts.get(ctx));
@@ -40,72 +41,65 @@ public class MiniGoPrintListener extends MiniGoBaseListener {
 
     @Override
     public void exitParams(MiniGoParser.ParamsContext ctx) {
-        newTexts.put(ctx, ")");
+        newTexts.put(ctx, ")\n");
         System.out.print(newTexts.get(ctx));
     }
 
     @Override
     public void enterCompound_stmt(MiniGoParser.Compound_stmtContext ctx) {
-//        StringBuilder sb = new StringBuilder();
-//        sb.append("\n{");
-        newTexts.put(ctx, "\n{\n");
+        newTexts.put(ctx, "{\n");
+        depth++;
         System.out.print(newTexts.get(ctx));
     }
 
     @Override
     public void exitCompound_stmt(MiniGoParser.Compound_stmtContext ctx) {
-//        StringBuilder sb = new StringBuilder();
-//        sb.append("\n{");
-        newTexts.put(ctx, "}");
+        depth--;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < depth; i++) {
+            sb.append("....");
+        }
+
+        sb.append("}\n");
+        newTexts.put(ctx, sb.toString());
         System.out.print(newTexts.get(ctx));
     }
 
     @Override
     public void enterExpr(MiniGoParser.ExprContext ctx) {
         String s1 = null, s2 = null, op = null;
-//        System.out.println("Hello enter expr");
-//        newTexts.put(ctx,"," + ctx);
         if (isBinaryOperation(ctx)) {
+            if (ctx.getChild(0) == ctx.IDENT()) {
+                s1 = ctx.IDENT().getText();
+                s2 = ctx.expr(0).getText();
+                op = ctx.getChild(1).getText();
+                newTexts.put(ctx, s1 + " " + op + " " + s2 + "\n");
+                System.out.print(newTexts.get(ctx));
+            }
 
-            s1 = ctx.IDENT().getText();
-            s2 = ctx.expr(0).getText();
-            op = ctx.getChild(1).getText();
-
-            newTexts.put(ctx, "...." + s1 + " " + op + " " + s2 + "\n");
-            System.out.print(newTexts.get(ctx));
+            if (ctx.getChild(0) == ctx.left) {
+                s1 = ctx.left.getText();
+                s2 = ctx.right.getText();
+                op = ctx.getChild(1).getText();
+                newTexts.put(ctx, "(" + s1 + " " + op + " " + s2 + ")\n");
+                System.out.print(newTexts.get(ctx));
+            }
         }
 
         if (isPrefixOperation(ctx)) {
-            StringBuilder sb = new StringBuilder();
-            sb.append("....");
-            sb.append(ctx.op.getText());
-//            sb.append(ctx)
-//            s1 = ctx.op.getText();
-            newTexts.put(ctx, sb.toString());
-//            System.out.print(s1);
+            newTexts.put(ctx, ctx.getText() + "\n");
+            System.out.print(newTexts.get(ctx));
         }
-
-
-        if (isSingleIdent(ctx)) {
-            newTexts.put(ctx, ctx.getText());
-
-//            System.out.print(newTexts.get(ctx));
-        }
-        if (isSingleExpr(ctx)) {
-//            System.out.print("Call");
-//            newTexts.put(ctx, ctx.getText());
-//            System.out.print(newTexts.get(ctx));
-        }
-
-//        if(M)
     }
 
-    public void exitExpr(MiniGoParser.ExprContext ctx){
-        String s1 = null, s2 = null, op = null;
-
-
-
-
+    @Override
+    public void enterStmt(MiniGoParser.StmtContext ctx) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < depth; i++) {
+            sb.append("....");
+        }
+        newTexts.put(ctx, sb.toString());
+        System.out.print(newTexts.get(ctx));
     }
 
     public boolean isBinaryOperation(MiniGoParser.ExprContext ctx) {
@@ -113,50 +107,12 @@ public class MiniGoPrintListener extends MiniGoBaseListener {
     }
 
     public boolean isPrefixOperation(MiniGoParser.ExprContext ctx) {
-        return ctx.getChildCount() == 2 && ctx.getChild(0) != ctx.expr() && ctx.getChild(0) != ctx.IDENT();
+        return ctx.getChildCount() == 2 && ctx.getChild(0) != ctx.IDENT() && ctx.getChild(0) != ctx.expr();
     }
-
-    public boolean isSingleExpr(MiniGoParser.ExprContext ctx) {
-        return ctx.getChildCount() == 1 && ctx.getChild(0) != ctx.IDENT();
-    }
-
-    public boolean isSingleIdent(MiniGoParser.ExprContext ctx){
-        return ctx.getChildCount() == 1 && ctx.getChild(0) == ctx.IDENT();
-    }
-
-    public boolean isIdentExpr(MiniGoParser.ExprContext ctx) {
-        return ctx.getChildCount() == 2 && ctx.getChild(0) != ctx.expr() && ctx.getChild(1) == ctx.expr();
-    }
-
-
-    @Override
-    public void enterDecl(MiniGoParser.DeclContext ctx) {
-//        System.out.println("Hello enter decl");
-//        newTexts.put(ctx, "enter decl " );
-//        System.out.println(newTexts.get(ctx));
-//        newTexts.put(ctx, "%%" + ctx.fun_decl());
-    }
-
 
     @Override
     public void exitFun_decl(MiniGoParser.Fun_declContext ctx) {
-//        System.out.println("Hello exit fun decl");
-//        newTexts.put(ctx, "++++"+ctx.getText());
-//        System.out.println(newTexts.get(ctx));
+        newTexts.put(ctx, "\n");
+        System.out.print(newTexts.get(ctx));
     }
-
-    @Override
-    public void enterVar_decl(MiniGoParser.Var_declContext ctx) {
-//        System.out.println("Hello enter var decl");
-//        newTexts.put(ctx, "..." + ctx.getText());
-//        System.out.println(newTexts.get(ctx));
-    }
-
-    @Override
-    public void exitVar_decl(MiniGoParser.Var_declContext ctx) {
-//        System.out.println("Hello exit var decl");
-//        newTexts.put(ctx, "\n" + ctx.getText());
-    }
-
-
 }
